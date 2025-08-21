94a1712b4d1aa7f85a283a5f976a17f04a610bc2
Jairo Guerrero <softwavecr@hotmail.com>
2025-08-20 21:36:44 -0400

    Layout with Navigation

---
+++ frontend/client/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div>
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <main style={{ padding: '1rem' }}>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 
@@ -8,9 +9,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+// client/src/main.jsx
